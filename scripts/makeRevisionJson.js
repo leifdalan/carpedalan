fa3c3ac2b0cda9619728d0ea4252cf448181afda
Leif Dalan <leifdalan@gmail.com>
2018-12-10 23:15:12 -0800

    Changed template

--- scripts/makeRevisionJson.js
+++ scripts/makeRevisionJson.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 
 const { ECR_ENDPOINT, CIRCLE_SHA1 } = process.env;
 const json = {
+  executionRoleArn: 'arn:aws:iam::771396871964:role/ecsTaskExecutionRole',
   containerDefinitions: [
     {
       logConfiguration: {
@@ -16,8 +17,6 @@ const json = {
       },
       image: `${ECR_ENDPOINT}/carpedev:${CIRCLE_SHA1}`,
       name: 'carpedalan',
-      memoryReservation: 512,
-      cpu: 256,
       portMappings: [
         {
           containerPort: 80,
@@ -27,10 +26,13 @@ const json = {
       ],
     },
   ],
+  memory: '512',
+  cpu: '256',
   taskRoleArn: 'arn:aws:iam::771396871964:role/ecsTaskExecutionRole',
   family: 'carpedalan',
   networkMode: 'awsvpc',
   volumes: [],
+  requiresCompatibilities: ['FARGATE'],
 };
 
 fs.writeFileSync('container-definition.json', JSON.stringify(json), 'utf8');
