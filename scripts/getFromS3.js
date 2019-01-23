2e27e7b804356e323ceb8e587d171c7cd254c9e8
Leif Dalan <leifdalan@gmail.com>
2019-01-22 16:53:57 -0800

    Added friggen node script for getting s3 asset

---
+++ scripts/getFromS3.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const fs = require('fs'); // from node.js
+
+const AWS = require('aws-sdk'); // from AWS SDK
+
+const s3 = new AWS.S3({ signatureVersion: 'v4' });
+
+const params = {
+  Bucket: 'configurations-pipeline',
+  Key: '.env',
+};
+const file = fs.createWriteStream('.env');
+s3.getObject(params)
+  .createReadStream()
+  .pipe(file);
