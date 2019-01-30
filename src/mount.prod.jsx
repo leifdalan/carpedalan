aa17abe68da5f9eada87da49d2c0b3e995d4baa0
Leif Dalan <leifdalan@gmail.com>
2019-01-30 10:26:15 -0800

    fix mount

--- src/mount.prod.jsx
+++ src/mount.prod.jsx
@@ -1,14 +1,16 @@
 import 'babel-polyfill';
 import React from 'react';
 import DOM from 'react-dom';
-import LogRocket from 'logrocket';
-import * as Sentry from '@sentry/browser';
+// import LogRocket from 'logrocket';
+// import * as Sentry from '@sentry/browser';
 
 import App from '.';
 
 const isCi = window.__META__.ci; // eslint-disable-line
 
 if (!isCi) {
+  const LogRocket = require('logrocket');
+  const Sentry = require('@sentry/browser');
   LogRocket.init('ovx5ph/carpe-dalan');
   Sentry.init({
     dsn: 'https://56efb56b3ba44197ad36ba25fb1e64a6@sentry.io/1380053',
@@ -19,11 +21,6 @@ if (!isCi) {
     return data;
   });
 
-  /* eslint-disable no-underscore-dangle */
-  DOM.render(
-    <App {...window.__SESSION__} {...window.__META__} />,
-    document.getElementById('root'),
-  );
   /* eslint-disable no-console */
   if ('serviceWorker' in navigator) {
     console.log('CLIENT: service worker registration in progress.');
@@ -39,3 +36,9 @@ if (!isCi) {
     console.log('CLIENT: service worker is not supported.');
   }
 }
+
+/* eslint-disable no-underscore-dangle */
+DOM.render(
+  <App {...window.__SESSION__} {...window.__META__} />,
+  document.getElementById('root'),
+);
