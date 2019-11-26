cc12eeaef8aa78757ed81a4f6300d8f32c4c48e5
Leif Dalan <leifdalan@gmail.com>
2019-11-25 23:37:33 -0800

    changed build env

--- scripts/pipeline/build.sh
+++ scripts/pipeline/build.sh
@@ -3,7 +3,7 @@ docker build -t $ECR:$CI_COMMIT_SHA \
   -t app:latest \
   -f Dockerfile.prod \
   --target prod \
-  --build-arg CIRCLE_SHA1=$CIRCLE_SHA1 \
+  --build-arg CIRCLE_SHA1=$CI_COMMIT_SHA \
   --build-arg AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
   --build-arg AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
   --build-arg S3_ASSETS_BUCKET=$(pulumi stack output assetBucket)
