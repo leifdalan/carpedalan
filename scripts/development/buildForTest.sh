f795f8098a7126bf9864d600c34af873e74c6ae0
Leif Dalan <leifdalan@gmail.com>
2020-02-12 13:19:59 -0800

    Changed docker

--- scripts/development/buildForTest.sh
+++ scripts/development/buildForTest.sh
@@ -1,3 +1,4 @@
 docker build -t client:latest -f client/Dockerfile ./client
 docker build -t api:latest -f server/Dockerfile . --target=dev
 docker build -t registry.gitlab.com/carpedalan/carpedalan-web/cypress:latest -f cypress/Dockerfile ./cypress
+# docker run -v "$PWD/dist":/app/client/dist -e "ASSET_CDN_DOMAIN=cdn.carpe.dalan.dev" client:latest yarn build:prod
